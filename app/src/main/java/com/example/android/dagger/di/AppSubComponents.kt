0d6c291bb3a597ecb206f5f4e0ea2c45958194ef
Alfiyansya <achmadalfiansyah069@gmail.com>
2021-11-10 11:29:44 +0700

    Create Subcomponents and Scoping Subcomponents

---
+++ app/src/main/java/com/example/android/dagger/di/AppSubComponents.kt
@@ -0,0 +1,8 @@
+package com.example.android.dagger.di
+
+import com.example.android.dagger.registration.RegistrationComponent
+import dagger.Module
+import dagger.Subcomponent
+
+@Module(subcomponents = [RegistrationComponent::class])
+class AppSubComponents
